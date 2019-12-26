8797eb03b578c228be8f716f960187f08b3ad9c1
xingo <wolifexingo@gmail.com>
2019-12-26 09:48:07 +0800

    Site updated: 2019-12-26 09:48:07

---
+++ js/src/click_show_text.js
@@ -0,0 +1,33 @@
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array
+        ("苟", "利", "国", "家", "生", "死", "以", "岂", "因", "祸", "福", "避", "趋", "之");
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 5,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "#FF0000"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+            3000,
+            function() {
+                $i.remove();
+            });
+    });
+    setTimeout('delay()', 2000);
+});
+
+function delay() {
+    $(".buryit").removeAttr("onclick");
+}
